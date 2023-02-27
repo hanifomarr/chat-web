57b79d2a5bd14a042073dd4292867321f5698bf2
Hanif Omar <57039713+hanifomarr@users.noreply.github.com>
2023-02-27 18:02:15 +0800

    login page UI

---
+++ src/page/login/Login.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./login.scss"
+
+const Login = () => {
+    return (
+        <div className='login'>
+            <div className="card">
+                <div className="left">
+                    <h1>Connect Now!</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, quidem officiis? Ab dignissimos consequatur nisi?</p>
+                    <span>Dont have an account?</span>
+                    <button>Register</button>
+                </div>
+                <div className="right">
+                    <h1>Login</h1>
+                    <form>
+                        <input type="text" placeholder='Username' />
+                        <input type="password" placeholder='Password' />
+                        <button>Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
