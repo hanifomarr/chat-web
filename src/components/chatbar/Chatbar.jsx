2869e5d6fac170ba937b4f64d46367fd04f311c0
Hanif Omar <57039713+hanifomarr@users.noreply.github.com>
2023-03-01 11:01:00 +0800

    chatbar UI

--- src/components/chatbar/Chatbar.jsx
+++ src/components/chatbar/Chatbar.jsx
@@ -3,7 +3,68 @@ import './chatbar.scss'
 
 const chatbar = () => {
     return (
-        <div className="chatbar">Chatbar</div>
+        <div className="chatbar">
+            <div className="container">
+                <div className="item">
+                    <span>Suggested Friendlist</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/3543419/pexels-photo-3543419.jpeg" alt="" />
+                            <span>Name</span>
+                        </div>
+                        <div className="buttons">
+                            <button>Accept</button>
+                            <button>Reject</button>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/3543419/pexels-photo-3543419.jpeg" alt="" />
+                            <span>Name</span>
+                        </div>
+                        <div className="buttons">
+                            <button>Accept</button>
+                            <button>Reject</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="item">
+                    <span>Latest Activities</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/3543419/pexels-photo-3543419.jpeg" alt="" />
+                            <span>Name</span>
+                            <p>Do something notifications</p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="item">
+                    <span>Online</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/3543419/pexels-photo-3543419.jpeg" alt="" />
+                            <div className="online"></div>
+                            <span>Name</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/3543419/pexels-photo-3543419.jpeg" alt="" />
+                            <div className="online"></div>
+                            <span>Name</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/3543419/pexels-photo-3543419.jpeg" alt="" />
+                            <div className="online"></div>
+                            <span>Name</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
