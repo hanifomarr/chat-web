c9f3880dc9f44d2ef8ac4c4f72b5881c157f55a6
Hanif Omar <57039713+hanifomarr@users.noreply.github.com>
2023-03-01 16:59:55 +0800

    Stories UI

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,11 +1,55 @@
 import React from 'react'
+import Strories from '../../components/stories/Strories'
+import Post from '../../components/posts/Post'
 import "./home.scss"
 
 const home = () => {
     return (
         <div className="homepage">
-            <h1>Homepage</h1>
-
+            <Strories />
+            <Post />
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
+            <h1>HOME</h1>
         </div>
     )
 }
